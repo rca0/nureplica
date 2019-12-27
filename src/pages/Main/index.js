48be951d5bddfc3854ab997c672f6bfa30587fc7
Ruan Arcega <ruanarcega@ip-192-168-0-104.ec2.internal>
2019-12-27 00:26:48 -0300

    feat: add tabs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
-import Header from '~/components/Header';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from "./styles";
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from "./styles";
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={26} color="#666"/>
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 1.040.612,30</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+                Transferência de R$ 4.000,00 recebida por Ruan Arcega hoje às 15:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
     </Container>
   );
 }
