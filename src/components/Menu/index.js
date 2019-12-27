23eb7653f6da59a3301c45177568b994a9d5d576
Ruan Arcega <ruanarcega@gmail.com>
2019-12-27 00:56:42 -0300

    feat: add nav styles in menu component

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import QRCode from 'react-native-qrcode';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Code } from './styles';
+import { Container, Code, Nav, NavItem, NavText } from './styles';
 
 export default function Menu() {
     return (
@@ -16,6 +17,24 @@ export default function Menu() {
                 />
             </Code>
 
+            <Nav>
+                <NavItem>
+                    <Icon name="help-outline" size={20} color="#FFF"/>
+                    <NavText>Me ajuda</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="person-outline" size={20} color="#FFF"/>
+                    <NavText>Perfil</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="credit-card" size={20} color="#FFF"/>
+                    <NavText>Configurar cartão</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="smartphone" size={20} color="#FFF"/>
+                    <NavText>Configurações do app</NavText>
+                </NavItem>
+            </Nav>
         </Container>
     );
 }
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,13 +1,32 @@
 import styled from 'styled-components/native';
+import { StyleSheet } from 'styled-components/native';
 
-export const Container = styled.ScrollView.attrs({
-    contentContainerStyle: { alignItems: 'center' },
-})`
+export const Container = styled.ScrollView`
   margin: 0 30px;
 `;
 
 export const Code = styled.View`
   background: #FFF;
   padding: 10px;
+  align-self: center;
 `;
 
+export const Nav = styled.View`
+  margin-top: 30px;
+  border-top-width: ${StyleSheet.hairlineWidth}px;
+  border-top-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 12px 0;
+  border-bottom-width: ${StyleSheet.hairlineWidth}px;
+  border-bottom-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavText = styled.Text`
+  font-size: 15px;
+  color: #FFF;
+  margin-left: 20px;
+`;
