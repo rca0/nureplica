b381c2a9f23b9409d795c228b8f94e2c3094568e
Ruan Arcega <ruanarcega@gmail.com>
2019-12-27 01:39:06 -0300

    feat: add animate resource

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,9 +5,14 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Code, Nav, NavItem, NavText, SignOutButtom, SignOutButtomText } from './styles';
 
-export default function Menu() {
+export default function Menu({ translateY }) {
     return (
-        <Container>
+        <Container style={{
+            opacity: translateY.interpolate({
+                inputRange: [0, 150],
+                outputRange: [0, 1],
+            }),
+        }}>
             <Code>
                 <QRCode
                     value="https://ruanc.net"
