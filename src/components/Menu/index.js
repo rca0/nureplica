57f291a5f2c0756582173fc3f3a1bbb04d11def5
Ruan Arcega <ruanarcega@gmail.com>
2019-12-27 01:50:20 -0300

    fix: menu component

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,47 +3,48 @@ import React from 'react';
 import QRCode from 'react-native-qrcode';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Code, Nav, NavItem, NavText, SignOutButtom, SignOutButtomText } from './styles';
+import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles';
 
 export default function Menu({ translateY }) {
-    return (
-        <Container style={{
-            opacity: translateY.interpolate({
-                inputRange: [0, 150],
-                outputRange: [0, 1],
-            }),
-        }}>
-            <Code>
-                <QRCode
-                    value="https://ruanc.net"
-                    size={80}
-                    fgColor="#FFF"
-                    bgColor="#8B10AE"
-                />
-            </Code>
+  return (
+    <Container style={{
+      opacity: translateY.interpolate({
+        inputRange: [0, 150],
+        outputRange: [0, 1],
+      }),
+    }}
+    >
+      <Code>
+        <QRCode
+          value="https://ruanc.net"
+          size={80}
+          fgColor="#fff"
+          bgColor="#8B10AE"
+        />
+      </Code>
 
-            <Nav>
-                <NavItem>
-                    <Icon name="help-outline" size={20} color="#FFF"/>
-                    <NavText>Me ajuda</NavText>
-                </NavItem>
-                <NavItem>
-                    <Icon name="person-outline" size={20} color="#FFF"/>
-                    <NavText>Perfil</NavText>
-                </NavItem>
-                <NavItem>
-                    <Icon name="credit-card" size={20} color="#FFF"/>
-                    <NavText>Configurar cartão</NavText>
-                </NavItem>
-                <NavItem>
-                    <Icon name="smartphone" size={20} color="#FFF"/>
-                    <NavText>Configurações do app</NavText>
-                </NavItem>
-            </Nav>
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
 
-            <SignOutButtom onPress={() => {}}>
-                <SignOutButtomText>SAIR DO APP</SignOutButtomText>
-            </SignOutButtom>
-        </Container>
-    );
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+      </SignOutButton>
+    </Container>
+  );
 }
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,14 +1,14 @@
+import { Animated, StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
-import { StyleSheet, Animated } from 'react-native';
 
 export const Container = styled(Animated.ScrollView)`
   margin: 0 30px;
 `;
 
 export const Code = styled.View`
-  background: #FFF;
   padding: 10px;
   align-self: center;
+  overflow: hidden;
 `;
 
 export const Nav = styled.View`
@@ -31,18 +31,18 @@ export const NavText = styled.Text`
   margin-left: 20px;
 `;
 
-export const SignOutButtom = styled.TouchableOpacity`
+export const SignOutButton = styled.TouchableOpacity`
   border-width: ${StyleSheet.hairlineWidth}px;
   border-color: rgba(255, 255, 255, 0.8);
   border-radius: 4px;
   justify-content: center;
   align-items: center;
-  padding: 12px; 
+  padding: 12px;
   margin-top: 15px;
 `;
 
-export const SignOutButtomText = styled.Text`
-   font-size: 13px;
-   color: #FFF;
-   font-weight: bold;
- `;
+export const SignOutButtonText = styled.Text`
+  color: #FFF;
+  font-weight: bold;
+  font-size: 13px;
+`;
