5fabfc3ffba3d782b8363384ef0c44fb7289802e
Ruan Arcega <ruanarcega@gmail.com>
2019-12-27 01:55:24 -0300

    fix: indetation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Top, Logo, Title } from "./styles";
+import { Container, Top, Logo, Title } from './styles';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -9,11 +9,11 @@ import logo from '~/assets/Nubank_Logo.png';
 export default function Header() {
   return (
     <Container>
-        <Top>
-            <Logo source={logo} />
-            <Title>Ruan</Title>
-        </Top>
-        <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
-     </Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>Ruan</Title>
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
+    </Container>
   );
 }
