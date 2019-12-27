4188b54ba0dbf1951d75bad3c35d3294c787c6f0
Ruan Arcega <ruanarcega@ip-192-168-0-104.ec2.internal>
2019-12-27 00:02:21 -0300

    feat: add react-native-iphone-x-helper

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Container, Top, Logo, Title } from "./styles";
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import logo from '~/assets/Nubank_Logo.png';
 
